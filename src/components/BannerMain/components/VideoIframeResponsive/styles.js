b88d0e17f50cd2675f4eedf21e6d2b1f22364e97
Nathaly Mesquita <nnathalygm@gmail.com>
2020-07-30 17:42:21 -0300

    Modificando o Footer

--- src/components/BannerMain/components/VideoIframeResponsive/styles.js
+++ src/components/BannerMain/components/VideoIframeResponsive/styles.js
@@ -7,12 +7,14 @@ export const VideoContainer = styled.div`
   padding-top: 56.25%;
   @media (max-width: 800px) {
     display: none;
+
   }
   
 `;
 
 export const ResponsiveIframe = styled.iframe`
   position: absolute;
+  background-position: center center;
   top: 0;
   left: 0;
   bottom: 0;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { FooterBase } from './styles';
+import { FooterBase, ExternalLink } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
-      <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
-      </p>
+      <ExternalLink href="https://github.com/nathy-mesquita">
+        Nathaly Mesquita
+      </ExternalLink>
+        © 2020. Desenvolvido com ❤ durante a
+      <ExternalLink href="https://www.alura.com.br/">
+        Imersão React da Alura
+      </ExternalLink>
+
     </FooterBase>
   );
 }
+{/* <a href="https://www.alura.com.br/">
+  <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+</a> */}
 
 export default Footer;
